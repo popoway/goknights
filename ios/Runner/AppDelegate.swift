9957e343481565802b685ebc33845b328e202f9f
Ming Lei <31576265+popoway@users.noreply.github.com>
2024-01-07 12:38:12 -0500

    feat: adaptive splash screen

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    window?.tintColor = UIColor(red: 231/255, green: 25/255, blue: 57/255, alpha: 1) // #E71939
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
